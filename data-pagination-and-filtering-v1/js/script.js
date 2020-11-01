f3cfef76f07e61f1975ab36a761d81f0b01b73da
MaperPario <69398573+MaperPario@users.noreply.github.com>
2020-11-01 12:05:04 -0800

    Added showPage function
    Functional and usable without any problems.

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -2,7 +2,8 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
+const items_per_page = 9;
+const studentList = document.querySelector('.student-list');
 
 
 /*
@@ -14,11 +15,32 @@ For assistance:
 
 
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Created a showPage function, displaying 9 students per page
 */
+function showPage(list, page) {
+   const startIndex = (page * items_per_page) - items_per_page;
+   const endIndex = startIndex + items_per_page;
 
+studentList.innerHTML = '';
 
+   for (let i = 0; i < list.length; i++) {
+      if(i >= startIndex && i < endIndex) {
+         studentList.insertAdjacentHTML("beforeend",
+          `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`);
+      }
+   } 
+}
+
+showPage(data, 1);
 
 /*
 Create the `addPagination` function
